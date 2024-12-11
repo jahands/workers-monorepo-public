1b77f6970fd6f4bc411fc66006e10db978805145
Jacob Hands <jacob@gogit.io>
2024-12-11 04:45:09 -0600

    fix comment

--- apps/kv-proxy-example/src/index.ts
+++ apps/kv-proxy-example/src/index.ts
@@ -24,7 +24,7 @@ const retryOpts: RetryOptions = {
 const app = new Hono<App>()
 	.use(
 		'*',
-		// add some metadata used in trac
+		// add some metadata used in traces
 		useMeta,
 		useDefaultCors(),
 		useSentry(initSentry, 'http.server'),
